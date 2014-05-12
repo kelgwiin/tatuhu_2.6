9c0dafb63f009898036a1b0b863e2ff32f699d6b
root <root@serverubuntu-ProLiant-ML110-G7>
2014-05-12 09:58:27 -0430

    Romovido Tatuhu

--- TatuHu/loginTatuHu/recuperarcontrasenia.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('../../aula_tatu/config.php');
-echo "<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\" />";
-
-
-?>
--- TatuHu/mobile/plantilla/banner1.php
+++
@@ -1,6 +0,0 @@
-<div data-theme="a" data-role="header" id="header">
-    <div style="width: 72px; height: 72px; position: relative; margin:0 auto;">
-        <img src="images/logo.png" alt="image"/>
-    </div>
-    <h3> Proyecto Tatu H&uacute; </h3>
-</div>
